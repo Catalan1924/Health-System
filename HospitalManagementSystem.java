d85f2ed2740adfb28f4cf0f81ddbcc16b22049f7
Mesh😁 <oduor2shem@gmail.com>
2026-02-25 11:46:10 +0300

    add core classes and interfaces for health management system including Person, Doctor, Nurse, Patient, Appointment, BillingService, and Department

--- HospitalManagementSystem.java
+++ HospitalManagementSystem.java
@@ -256,3 +256,47 @@ class Department {
     }
 }
 
+
+public class HospitalManagementSystem {
+    public static void main(String[] args) {
+
+       
+        Doctor doc1 = new Doctor("D001", "Amina Hassan", "0712345678", "Cardiology", 180000);
+        Nurse nurse1 = new Nurse("N001", "Brian Otieno", "0798765432", "Emergency", 500, 160);
+        Patient patient1 = new Patient("P001", "Felijoe Oqueer", "0700111222", 18, "Fever");
+
+       
+        Person p1 = doc1;   
+        Person p2 = nurse1;   
+        Person p3 = patient1;
+
+        System.out.println("PERSON DETAILS (Inheritance + Overriding)");
+        System.out.println(p1.getDetails());
+        System.out.println(p2.getDetails());
+        System.out.println(p3.getDetails());
+
+        // Call methods
+        System.out.println("\n STAFF PAYROLL (Interfaces)");
+        doc1.pay();
+        nurse1.pay();
+
+        System.out.println("\nMEDICAL WORKFLOW");
+        nurse1.assistDoctor(doc1);
+        doc1.diagnose(patient1);
+
+        System.out.println("\nAPPOINTMENT (Interface: Schedulable)");
+        Appointment appt1 = new Appointment("A100", patient1, doc1, "2026-03-01", "10:30 AM");
+        appt1.schedule();
+        appt1.reschedule("2026-03-02", "2:00 PM");
+
+        System.out.println("\nSERVICES (Abstract Class HospitalService) ");
+        BillingService billing = new BillingService("Patient Billing", patient1, 3500);
+        billing.executeService();
+
+        System.out.println("\nDEPARTMENT ");
+        Department dept = new Department("Cardiology");
+        dept.showDepartment();
+
+        System.out.println("\nSystem demo complete - all features showcased successfully!");
+    }
+}
